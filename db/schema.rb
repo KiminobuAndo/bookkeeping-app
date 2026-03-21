04e2b90dc69080383a275495097284e1bb48609a
KiminobuAndo <habit130305@gmail.com>
2026-03-22 08:01:33 +0900

    fix: update schema after answers migration (#15)

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_21_132551) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_21_172835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "question_id", null: false
+    t.string "selected_side", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id", "question_id"], name: "index_answers_on_user_id_and_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name", null: false
@@ -44,5 +55,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_21_132551) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "questions", "courses"
 end
